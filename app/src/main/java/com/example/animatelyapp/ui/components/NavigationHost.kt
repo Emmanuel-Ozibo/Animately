b3abac1e7617a8f0c490692ef206ee2176c20bf2
EmmanuelOzibo <eozibo@league.com>
2024-01-27 22:51:15 +0100

    Implemented ShipmentSummaryScreen animations

--- app/src/main/java/com/example/animatelyapp/ui/components/NavigationHost.kt
+++ app/src/main/java/com/example/animatelyapp/ui/components/NavigationHost.kt
@@ -1,5 +1,10 @@
 package com.example.animatelyapp.ui.components
 
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
